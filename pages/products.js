d24c5f0812300408e93f660ddedd22bc2cba6ec5
@hashmiajith <hashmiajith@gmail.com>
2023-05-21 23:20:01 +0530

    initial commmit and side bar added and google auth added

---
+++ pages/products.js
@@ -0,0 +1,12 @@
+import Layout from '@/components/layout'
+import React from 'react'
+
+const Products = () => {
+  return (
+    <Layout>
+        Products
+    </Layout>
+  )
+}
+
+export default Products
